d48b2fc9996d66f6ec1806c8cec31d9dfae38285
olisagold <olisaejezie@gmail.com>
2024-02-18 08:10:33 -0800

    first commit

---
+++ midsemester.html/learn.js/learning.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html>
+ <body>
+ <h2>My First JavaScript</h2>
+ <button type="button"
+ onclick="document.getElementById('demo').innerHTML = Date()">
+ Click me to display Date and Time.</button>
+ <p id="demo"></p>
+ <script> 
+    function myfunction(p1, p2) {
+        return p1 * p2;
+    }
+    let result = myFunction(4, 3);
+    document.getElementById("demo").innerHTML = result;
+    const hour = new date().getHours();
+ </script>
+ </body>
+ </html>
